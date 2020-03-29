92302a55c384c9cc1ed554f1fa0b98a56c2f6cc2
yuivy <sy2021x@gmail.com>
2020-03-29 12:31:49 +0000

    投稿一覧画面、投稿詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index' => 'posts#index'
+  # 投稿一覧ページ
+  get 'posts/:id' => 'posts#show'
+  # 投稿詳細ページ
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
