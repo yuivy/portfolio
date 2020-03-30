d3350902deb48c74cc75d003f12acc28b2a03b84
yuivy <sy2021x@gmail.com>
2020-03-30 06:56:14 +0000

    新規投稿ページの作成、投稿データの保存、投稿一覧画面への転送

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
   # 投稿一覧ページ
+  get 'posts/new' => 'posts#new'
+  # 新規投稿ページ
+  post 'posts/create' => 'posts#create'
+  # 投稿内容を受け取る
   get 'posts/:id' => 'posts#show'
   # 投稿詳細ページ
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
