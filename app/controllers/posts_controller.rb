23b9befebc581a54924defd686d084a763c437e5
yuivy <sy2021x@gmail.com>
2020-03-30 10:28:40 +0000

    投稿成功、失敗の記述+変数flash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,17 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice]="投稿に成功しました"
+      redirect_to("/posts/index")
+    else
+      render("/posts/new")
+    end
   end
   
   def edit
@@ -23,9 +28,14 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
-    
-    redirect_to("/posts/index")
+    if @post.save
+        flash[:notice]="編集に成功しました"
+        # 変数flash
+      redirect_to("/posts/index")
+    else
+      render("/posts/edit")
+    #   renderメソッドをつけることで直前の投稿を表示できる
+    end
   end
   
   def destroy
