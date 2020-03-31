edb79a73ae9701209ec5e77401894dd3e02e9192
yuivy <sy2021x@gmail.com>
2020-03-31 06:58:10 +0000

    フラッシュの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice]="ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("/users/new")
+    end
   end
 end
