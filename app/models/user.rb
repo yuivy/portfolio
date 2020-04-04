566f0aed62f834abf8299e244a88e342e7904a87
yuivy <sy2021x@gmail.com>
2020-04-04 05:14:26 +0000

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
-  
+  validates :introduce, {presence: true, length:{maximum: 255}}
+  validates :product, {presence: true, length:{maximum: 50}}
+  validates :game_mode, {presence: true, length:{maximum: 50}}
   def posts
     return Post.where(user_id: self.id)
   end
