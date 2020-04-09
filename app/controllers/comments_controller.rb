ab55834cbccacf5bca3a993842442e528722f0bf
yuivy <sy2021x@gmail.com>
2020-04-09 08:30:08 +0000

    createアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.new(comment_params)
+    @comment.user_id = @current_user.id
+    if @comment.save
+      flash[:success] = "コメントしました"
+      redirect_to("/posts/index")
+
+    else
+      flash[:danger] = "コメントできませんでした"
+      render()
+      # コメント入力viewに戻る
+    end
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
