ed81d439109c462d42cbfb619459b90dd61322eb
yuivy <sy2021x@gmail.com>
2020-04-09 06:49:19 +0000

    コメントモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_045515) do
+ActiveRecord::Schema.define(version: 2020_04_09_064824) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -31,4 +41,6 @@ ActiveRecord::Schema.define(version: 2020_04_04_045515) do
     t.string "game_mode"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
