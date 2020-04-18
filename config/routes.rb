196350336621e8e8b01c6d422e5645d51f54f9d3
yuivy <sy2021x@gmail.com>
2020-04-18 09:29:12 +0000

    userのリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
 
-    resources :comments, only: [:create, :update]
-  # end
+  resources :comments, only: [:create, :update, :edit, :destroy]
+  get 'comments/:id/edit' => "comments#edit"
+  # コメント編集ページ
+  post 'comments/:id/destroy' => 'comments#destroy'
   
-  get '/' => 'home#top'
-  # トップページ
-  get '/about' => 'home#about'
-  # サービス紹介ページ
-  get 'users/index' => 'users#index'
-  # ユーザー一覧ページ
-  get 'users/:id' => 'users#show'
-  # ユーザー詳細ページ
-  post 'users/create' => 'users#create'
-  # ユーザーの保存
-  get '/signup' => 'users#new'
-  # 新規ユーザー登録
-  get 'users/:id/edit' => 'users#edit'
-  # ユーザーの編集
-  post 'users/:id/update' => 'users#update'
-  # ユーザー編集後の内容を受け取る
+  resources :users
+  get '/login' => 'users#login_form'
+  # ユーザーのログイン
+  post '/login' => 'users#login'
+  # ログイン情報を受け取る
+  post '/logout' => 'users#logout'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get 'posts/index' => 'posts#index'
   # 投稿一覧ページ
   get 'posts/new' => 'posts#new'
@@ -33,14 +27,8 @@ Rails.application.routes.draw do
   # 編集後の投稿内容を受け取る
   post 'posts/:id/destroy' => 'posts#destroy'
   # 投稿内容の削除
-  get '/login' => 'users#login_form'
-  # ユーザーのログイン
-  post '/login' => 'users#login'
-  # ログイン情報を受け取る
-  post '/logout' => 'users#logout'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  get 'comments/:id/edit' => "comments#edit"
-  # コメント編集ページ
-  post 'comments/:id/destroy' => 'comments#destroy'
+  get '/' => 'home#top'
+  # トップページ
+  get '/about' => 'home#about'
+  # サービス紹介ページ
 end
